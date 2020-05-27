79ec9bf49174c7609146b921d2dec7ef77272d23
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2020-05-27 15:53:26 +0200

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int chay = 0;
+
+    while(chay <= 5){
+        cout << "chay is " << chay << endl;
+        chay = chay + 1;
+    }
+    return 0;
+}
