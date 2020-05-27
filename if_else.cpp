319083d720f40110c323d6e660f923a2e1ef8b5d
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2020-05-27 15:41:14 +0200

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x = 11;
+
+    if(x == 10){
+    cout << "is 10!" << endl;
+    }
+    else{
+    cout << "is not 10!" << endl;
+    }
+    return 0;
+}
