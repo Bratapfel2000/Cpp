95c168ff662936da69c03db311ea328f257120dc
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2020-07-02 17:24:37 +0200

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 14;
+
+    switch(x){
+        case 4:
+            cout << "case 4" << endl;
+            break;
+        case 14:
+            cout << "case 14" << endl;
+            break;
+        case 153:
+            cout << "case 153" << endl;
+            break;
+        default:
+            cout << "wrong case" << endl;
+
+
+    }
+
+}
